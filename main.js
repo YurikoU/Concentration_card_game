7c77bef4d6d32badea316661a620c7b6cb4e9bcd
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-08-31 10:23:22 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,20 @@
 
 
 
+//Store a pair of cards
+let cards = [
+    "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", 
+    "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", 
+];
 
+let $field = document.getElementById("field");
+
+for ( let i = 0; i < cards.length; i++ ) {
+    let element = document.createElement("div");
+
+    element.className = "card";//Add class name
+    element.innerHTML = cards[ i ];//Set the text
+    element.index     = i;
+
+    $field.appendChild( element );
+}
